60ec9b707d7b56564bde424f186949d3f175f373
Bohdan Lutsyk <26075550+barchiso@users.noreply.github.com>
2025-02-12 00:21:42 +0200

    add hw to lesson #27. (#5)

--- pom/pages/HomePage.ts
+++ pom/pages/HomePage.ts
@@ -3,10 +3,12 @@ import { Locator, Page } from '@playwright/test';
 export default class HomePage {
 	readonly page: Page;
 	readonly signUpButton: Locator;
+	readonly signInButton: Locator;
 
 	constructor(page: Page) {
 		this.page = page;
 		this.signUpButton = page.locator('//button[text()="Sign up"]');
+		this.signInButton = page.locator('//button[text()="Sign In"]');
 	}
 
 	async open() {
@@ -15,5 +17,9 @@ export default class HomePage {
 
 	async clickSignUpButton() {
 		await this.signUpButton.click();
+	}
+
+	async clickSignInButton() {
+		await this.signInButton.click();
 	}
 }
